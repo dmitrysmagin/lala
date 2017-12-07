64eaf484de999dc07f985e367e649ef4d7375e80
Dmitry Smagin <exmortis@yandex.ru>
2017-12-07 17:14:25 +0300

    Add simple build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CFLAGS="-std=c99 -g -Wall -Wextra -Wno-unused-parameter -Wno-unused-function `sdl-config --cflags`"
+LDFLAGS="`sdl-config --libs` -lm"
+
+gcc	src/engine.c src/fmengine.c src/lala.c \
+	-o lala $CFLAGS $LDFLAGS
